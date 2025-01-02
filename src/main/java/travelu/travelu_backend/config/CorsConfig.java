40b8cc9572895f2375cc8e73310b53dbf403b1e0
Lauda D Raka <96691604+elderaka@users.noreply.github.com>
2025-01-02 10:50:24 +0700

    Changed CORS to all origins

--- src/main/java/travelu/travelu_backend/config/CorsConfig.java
+++ src/main/java/travelu/travelu_backend/config/CorsConfig.java
@@ -1,21 +1,14 @@
 package travelu.travelu_backend.config;
 
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.auditing.DateTimeProvider;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.time.OffsetDateTime;
-import java.util.Optional;
-
-
 @Configuration
 public class CorsConfig {
+    public CorsConfig() {
+    }
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
@@ -23,9 +16,9 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:9000")
-                        .allowedMethods("*")
-                        .allowedHeaders("*")
+                        .allowedOrigins("*") // Allow all origins
+                        .allowedMethods("*") // Allow all methods (GET, POST, etc.)
+                        .allowedHeaders("*") // Allow all headers
                         .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Access-Control-Allow-Methods");
             }
         };
